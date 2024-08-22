d48fae4ec7f75fa10107a68b7f7f98a37e787cf6
PhillipDaum <philgdaum@gmail.com>
2024-08-22 12:16:25 -0600

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,56 @@
+const userArray = [];
+const summaryArea = document.getElementById("summary");
+
+const taxRates = {
+    NC: .22,
+    ID: .19,
+    MD: .31,
+    NM: .29,
+    AZ: .21,
+    GA: .25,
+}
+const marginalTaxRates = {
+    NC: .32,
+    ID: .39,
+    MD: .35,
+    NM: .32,
+    AZ: .31,
+    GA: .35,
+}
+
+// form submission
+const submitForm = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const dataObject = Object.fromEntries(data.entries());
+    userArray.push(dataObject);
+    console.log("line 8", userArray);
+    console.log(calculate(userArray));
+    summaryArea.innerHTML = calculate(userArray);
+}
+document.querySelector("#form").addEventListener("submit", submitForm);
+
+const calculate = (array) => {
+    let region = array[0].state;
+    let annualSalary;
+    if (array[0].per === "annual") {
+        annualSalary = array[0].income;
+    } else {
+        annualSalary = array[0].income * 12;
+    }
+    // what about this syntax with the brackets? it's weird
+    let theTaxRate = taxRates[region];
+    let annualTaxAmount = annualSalary * theTaxRate;
+    let netPay = annualSalary - annualTaxAmount;
+    let monthlyNetPay = netPay/12;
+    let theMarginalTaxRate = marginalTaxRates[region];
+    let exampleMarginalRate = 100 * theMarginalTaxRate;
+    let exampleMarginalIncrease = 100 - exampleMarginalRate;
+    return `If you make <strong>$${annualSalary}</strong> a year in the region of <strong>${region}</strong>,
+ you will be taxed <strong>$${theTaxRate}</strong>. That means that your net pay will be <strong>$${netPay}</strong> 
+ per year, or <strong>$${monthlyNetPay}</strong> per month. Your average tax rate is <strong>${theTaxRate}%</strong> and your 
+ marginal tax rate is <strong>${theMarginalTaxRate}%</strong>. This marginal tax rate means that your immediate additional 
+ income will be taxed at this rate. For instance, an increase of <strong>$100</strong> in your salary will 
+ be taxed <strong>$${exampleMarginalRate}</strong>, hence, your net pay will only increase by <strong>$${exampleMarginalIncrease}</strong>.`
+}
+
