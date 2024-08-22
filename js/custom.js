36662e70b548e42a9a21b4652b3fd88982aa4811
PhillipDaum <philgdaum@gmail.com>
2024-08-22 13:53:59 -0600

    add pie chart

--- js/custom.js
+++ js/custom.js
@@ -24,8 +24,6 @@ const submitForm = (e) => {
     const data = new FormData(e.target);
     const dataObject = Object.fromEntries(data.entries());
     userArray.push(dataObject);
-    console.log("line 8", userArray);
-    console.log(calculate(userArray));
     summaryArea.innerHTML = calculate(userArray);
 }
 document.querySelector("#form").addEventListener("submit", submitForm);
@@ -46,11 +44,18 @@ const calculate = (array) => {
     let theMarginalTaxRate = marginalTaxRates[region];
     let exampleMarginalRate = 100 * theMarginalTaxRate;
     let exampleMarginalIncrease = 100 - exampleMarginalRate;
+    makeChart(theTaxRate*100)
     return `If you make <strong>$${annualSalary}</strong> a year in the region of <strong>${region}</strong>,
- you will be taxed <strong>$${theTaxRate}</strong>. That means that your net pay will be <strong>$${netPay}</strong> 
- per year, or <strong>$${monthlyNetPay}</strong> per month. Your average tax rate is <strong>${theTaxRate}%</strong> and your 
- marginal tax rate is <strong>${theMarginalTaxRate}%</strong>. This marginal tax rate means that your immediate additional 
+ you will be taxed <strong>$${annualTaxAmount}</strong>. That means that your net pay will be <strong>$${netPay}</strong> 
+ per year, or <strong>$${monthlyNetPay}</strong> per month. Your average tax rate is <strong>${theTaxRate*100}%</strong> and your 
+ marginal tax rate is <strong>${theMarginalTaxRate*100}%</strong>. This marginal tax rate means that your immediate additional 
  income will be taxed at this rate. For instance, an increase of <strong>$100</strong> in your salary will 
  be taxed <strong>$${exampleMarginalRate}</strong>, hence, your net pay will only increase by <strong>$${exampleMarginalIncrease}</strong>.`
 }
 
+const makeChart = (ratio) => {
+    console.log(ratio);
+    document.getElementById('chart').style.background = `conic-gradient(var(--piechart-02) ${ratio}%, var(--piechart-01) 0)`;
+}
+
+// 
